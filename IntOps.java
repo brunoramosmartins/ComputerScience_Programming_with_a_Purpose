111f77ff51b864bb733d6b6c2b4425fa9ff0bb0d
brunoramosmartins <brunoramosmartins1989@gmail.com>
2023-05-20 21:45:51 -0300

    Terceiro programa da semana. Trabalhando com variáveis Double.

--- IntOps.java
+++ IntOps.java
@@ -1,3 +1,25 @@
+/******************************************************************************
+ *  Compilation:  javac IntOps.java
+ *  Execution:    java IntOps a b
+ *
+ *  Illustrates the integer operations a + b, a * b, a / b, and a % b.
+ *
+ *  % java IntOps 1234 99
+ *  1234 + 99 = 1333
+ *  1234 * 99 = 122166
+ *  1234 / 99 = 12
+ *  1234 % 99 = 46
+ *  1234 = 12 * 99 + 46
+ *
+ *  % java IntOps 10 -3
+ *  10 + -3 = 7
+ *  10 * -3 = -30
+ *  10 / -3 = -3
+ *  10 % -3 = 1
+ *  10 = -3 * -3 + 1
+ *
+ ******************************************************************************/
+ 
 public class IntOps {
   public static void main(String[] args) {
     int a = Integer.parseInt(args[0]);
