cc88b39f614e062ea534e1a207a7b3773a7cef66
brunoramosmartins <brunoramosmartins1989@gmail.com>
2023-05-23 23:35:37 -0300

    Diversos exercícios simples do primeiro capítulo.

---
+++ SumOfSines.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+ *  Compilation:  javac SumOfSines.java
+ *  Execution:    java SumOfSines t
+ *
+ *  Read in an angle t (in degrees) and print sin(2t) + sin(3t).
+ *
+ *  % java SumOfSines 30
+ *  1.8660254037844386
+ *
+ ******************************************************************************/
+
+public class SumOfSines {
+  public static void main(String[] args) {
+
+    double degrees = Double.parseDouble(args[0]);
+    double radians = Math.toRadians(degrees); // método que converte graus para radianos
+    double sum = Math.sin(2 * radians) + Math.sin(3 * radians);
+
+    System.out.println(sum);
+  }
+}
