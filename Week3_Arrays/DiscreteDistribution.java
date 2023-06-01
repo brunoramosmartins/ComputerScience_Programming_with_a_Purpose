1b1703081fa1fe2244fbb7e9693dfe63a9f51d3c
brunoramosmartins <brunoramosmartins1989@gmail.com>
2023-06-01 16:10:56 -0300

    Revisão de array.

---
+++ Week3_Arrays/DiscreteDistribution.java
@@ -0,0 +1,7 @@
+public class DiscreteDistribution {
+    public static void main(String[] args) {
+
+        int m = Integer.parseInt(args[0]);
+        int[] n = Integer.parseInt(args[1]);
+    }
+}
