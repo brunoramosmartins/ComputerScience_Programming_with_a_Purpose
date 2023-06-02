bbb11c0abb87a95a6df1268f7ccb197de8bbf360
brunoramosmartins <brunoramosmartins1989@gmail.com>
2023-06-02 11:23:29 -0300

    Solução da tarefa Week3.

--- Week3_Arrays/DiscreteDistribution.java
+++ Week3_Arrays/DiscreteDistribution.java
@@ -2,6 +2,18 @@ public class DiscreteDistribution {
     public static void main(String[] args) {
 
         int m = Integer.parseInt(args[0]);
-        int[] n = Integer.parseInt(args[1]);
+        int n = args.length - 1;
+        int sum = 0;
+
+        int[] sequence = new int[n];
+        for (int i = 0; i < n; i++) {
+            sequence[i] = Integer.parseInt(args(i + 1));
+            sum += sequence[i];
+        }
+
+        for (int i = 0; i < m; i++) {
+            int rand = (int) (Math.random() * sum);
+            System.out.print()
+        }
     }
 }
