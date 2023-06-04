a0e4486b44f06c65a84d021a9641e55f7069b2c2
brunoramosmartins <brunoramosmartins1989@gmail.com>
2023-06-04 17:56:12 -0300

    Exercicio interessante para verificar quantidade de primos.

--- Week3_Arrays/PrimeSieveSolution.java
+++ Week3_Arrays/PrimeSieveSolution.java
@@ -38,27 +38,34 @@
  *
  ******************************************************************************/
 
-public class PrimeSieve {
 
-    public static boolean isPrime(int n) {
-        for (int i = 2; i * i <= n; i++) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
+public class PrimeSieveSolution {
     public static void main(String[] args) {
-
         int n = Integer.parseInt(args[0]);
-        int count = 0;
 
+        // initially assume all integers are prime
+        boolean[] isPrime = new boolean[n+1];
         for (int i = 2; i <= n; i++) {
-            if (isPrime(i)) {
-                count++;
+            isPrime[i] = true;
+        }
+
+        // mark non-primes <= n using Sieve of Eratosthenes
+        for (int factor = 2; factor*factor <= n; factor++) {
+
+            // if factor is prime, then mark multiples of factor as non-prime
+            // suffices to consider multiples factor, factor+1, ...,  n/factor
+            if (isPrime[factor]) {
+                for (int j = factor; factor*j <= n; j++) {
+                    isPrime[factor*j] = false;
+                }
             }
         }
 
-        System.out.println("The number of primes <= " + n + " is " + count);
+        // count primes
+        int primes = 0;
+        for (int i = 2; i <= n; i++) {
+            if (isPrime[i]) primes++;
+        }
+        System.out.println("The number of primes <= " + n + " is " + primes);
     }
 }
