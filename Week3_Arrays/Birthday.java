864ab2c853404c5595e7fc7499078c6fc2affd76
brunoramosmartins <brunoramosmartins1989@gmail.com>
2023-06-05 11:01:22 -0300

    Rascunho da solução do problema 3.

--- Week3_Arrays/Birthday.java
+++ Week3_Arrays/Birthday.java
@@ -3,8 +3,14 @@ public class Birthday {
 
         int n = Integer.parseInt(args[0]);
         int trials = Integer.parseInt(args[1]);
+        int[] qtd = new Int[n + 1]
+
 
         for (int i = 0; i < trials; i++) {
+            int bday = Math.random() * n + 1;
+
+            
+
             
         }
     }
