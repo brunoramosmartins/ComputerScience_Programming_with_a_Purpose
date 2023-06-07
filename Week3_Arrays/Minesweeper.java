9ba2b5b48d7352de6a3c469c4b37a7594ec8e4d3
brunoramosmartins <brunoramosmartins1989@gmail.com>
2023-06-06 21:18:41 -0300

    Resolução do exercício 4.

--- Week3_Arrays/Minesweeper.java
+++ Week3_Arrays/Minesweeper.java
@@ -3,7 +3,7 @@ public class Minesweeper {
 
         int m = Integer.parseInt(args[0]);
         int n = Integer.parseInt(args[1]);
-        int k = Integer.parseInt(args[1]);
+        int k = Integer.parseInt(args[2]);
 
         char[][] grid = new char[m][n];
 
@@ -18,6 +18,22 @@ public class Minesweeper {
         }
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
+
+                if (grid[i][j] != '*') {
+                    grid[i][j] = (char) 0;
+                    int countbomb = 0; 
+                    for (int a = 0; a <= 2; a++) {
+                        for (int b = 0; b <= 2; b++) {
+                            if ((i + a - 1) >= 0 && (i + a - 1) < m && (j + b - 1) >= 0 && (j + b - 1) < n) {
+                                if (grid[i + a - 1][j + b - 1] == '*') {
+                                    countbomb++;
+                                }
+                            }
+                        }
+                    }
+                    grid[i][j] = Character.forDigit(countbomb, 10);
+                    
+                }
                 System.out.print(grid[i][j] + "  ");
             }
             System.out.println();
