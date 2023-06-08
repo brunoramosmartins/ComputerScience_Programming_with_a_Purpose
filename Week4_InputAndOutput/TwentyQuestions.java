e8526dcb1e29339c1df0e0ee5b846d5301698310
brunoramosmartins <brunoramosmartins1989@gmail.com>
2023-06-07 23:57:03 -0300

    Código da aula.

---
+++ Week4_InputAndOutput/TwentyQuestions.java
@@ -0,0 +1,28 @@
+/******************************************************************************
+ *  Compilation:  javac TwentyQuestions.java
+ *  Execution:    java TwentyQuestions
+ *  Dependencies  StdIn.java
+ *
+ *  % java TwentyQuestions
+ *  I'm thinking of a number between 1 and 1,000,000
+ *  What's your guess? 500000
+ *  Too high
+ *  What's your guess? 250000
+ *  Too low
+ *  What's your guess? 375000
+ *  Too high
+ *  What's your guess? 312500
+ *  Too high
+ *  What's your guess? 300500
+ *  Too low
+ *  ...
+ *
+ ******************************************************************************/
+
+import edu.princeton.cs.algs4.StdIn;
+
+public class TwentyQuestions {
+    public static void main(String[] args) {
+
+    }
+}
