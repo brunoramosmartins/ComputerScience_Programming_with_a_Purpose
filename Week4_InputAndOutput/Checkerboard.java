d2624e4e4c699ac5907b6369d108b6b1ae96b693
brunoramosmartins <brunoramosmartins1989@gmail.com>
2023-06-14 17:56:15 -0300

    Exercício base para atividade da semana.

---
+++ Week4_InputAndOutput/Checkerboard.java
@@ -0,0 +1,28 @@
+/******************************************************************************
+ *  Compilation:  javac Checkerboard.java
+ *  Execution:    java Checkerboard n
+ *  Dependencies: StdDraw.java
+ *
+ *  Plots an n-by-n checkerboard.
+ *
+ ******************************************************************************/
+
+import edu.princeton.cs.algs4.*;
+
+public class Checkerboard {
+    public static void main(String[] args) {
+
+        int n = Integer.parseInt(args[0]);
+
+        StdDraw.setXscale(0, n);
+        StdDraw.setYscale(0, n);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((i + j) % 2 != 0) StdDraw.setPenColor(StdDraw.BLACK);
+                else                  StdDraw.setPenColor(StdDraw.RED);
+                StdDraw.filledSquare(i + 0.5, j + 0.5, 0.5);
+            }
+        }
+    }
+}
