d3082655bb8a1e87a74e4285cd8be8ffce8bd089
brunoramosmartins <brunoramosmartins1989@gmail.com>
2023-06-18 23:59:04 -0300

    Finlizando a semana 4.

---
+++ Week4_InputAndOutput/WorldMap.java
@@ -0,0 +1,35 @@
+import edu.princeton.cs.algs4.*;
+
+public class WorldMap {
+    public static void main(String[] args) {
+        
+        int Width = StdIn.readInt();
+        int Height = StdIn.readInt();
+
+        StdDraw.setCanvasSize(Width, Height);
+
+        StdDraw.setXscale(0, Width);
+        StdDraw.setYscale(0, Height);
+
+        StdDraw.enableDoubleBuffering();
+
+        while (!StdIn.isEmpty()) {
+
+            StdIn.readString();
+
+            int n = StdIn.readInt(); // number of vertices
+            double[] vx = new double[n]; // x points
+            double[] vy = new double[n]; // y points
+
+            for (int i = 0; i < n; i++) {
+                vx[i] = StdIn.readDouble();
+                vy[i] = StdIn.readDouble();
+            }
+
+            StdDraw.polygon(vx, vy);
+
+        }
+
+        StdDraw.show();
+    }
+}
